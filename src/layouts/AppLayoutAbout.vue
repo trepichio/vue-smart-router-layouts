64f775149e6e44e9dd28fbad6b3b2dd8cfd1a951
trepichio <trepichio@gmail.com>
2021-03-25 08:00:29 -0300

    add layouts with different header colors

---
+++ src/layouts/AppLayoutAbout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header class="header"></header>
+    <AppLayoutLinks />
+    <slot />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import AppLayoutLinks from "@/layouts/AppLayoutLinks";
+
+export default defineComponent({
+  name: "AppLayoutAbout",
+  components: {
+    AppLayoutLinks,
+  },
+});
+</script>
+
+<style lang="css" scoped>
+.header {
+  height: 5rem;
+  background-color: blue;
+}
+</style>
