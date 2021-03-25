a9093f01ee71f69c79e790ecfa0ec1484c8e37f5
trepichio <trepichio@gmail.com>
2021-03-25 07:53:02 -0300

    add Contacts page

---
+++ src/views/Contacts.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>This a contacts page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Contacts",
+
+  data() {
+    return {};
+  },
+});
+</script>
+
+<style lang="css" scoped></style>
