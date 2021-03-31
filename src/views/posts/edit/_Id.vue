5b75135c7ed85dc51684d188a6cdb7276027c2f3
trepichio <trepichio@gmail.com>
2021-03-31 16:55:19 -0300

    fix typo and add layout

--- src/views/posts/edit/_Id.vue
+++ src/views/posts/edit/_Id.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>This is a page of the post with id {{ $route.params.id }}</h1>
+    <h1>This is a page to edit post with id {{ $route.params.id }}</h1>
   </div>
 </template>
 
@@ -9,7 +9,7 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "PostEdit",
-
+  layout: "AppLayoutPosts",
   data() {
     return {};
   },
